6293d4d3ac948aee2f615870ac5211291e199067
Arisha-Akbar <arishaakbar713@gmail.com>
2026-08-03 12:13:31 +0500

    Refactor cookie options in JWT token functions to use maxAge instead of expires

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,10 +1,8 @@
-// create token and saving that in cookies
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
-  // Options for cookies
   const options = {
-    expires: new Date(Date.now + 90 * 24 * 60 * 60 * 1000),
+    maxAge: 90 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: process.env.NODE_ENV === "PRODUCTION" ? "none" : "lax",
     secure: process.env.NODE_ENV === "PRODUCTION",
